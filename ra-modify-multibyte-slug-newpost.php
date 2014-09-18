d969b2aed4501becd6f5d34afed756fab0a447a1
skuramoto <skuramoto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-18 10:52:25 +0000

    1st release.
    git-svn-id: https://plugins.svn.wordpress.org/ra-mod-multibyt-slug/trunk@992492 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ra-modify-multibyte-slug-newpost.php
@@ -0,0 +1,46 @@
+<?php 
+/*
+Plugin Name: RA - Mod Multibyt Slug
+Description: When the multi-byte character is used in slug of new creation post, it changes so that post_type and post_ID may be used.
+Author: Rasin (skuraomoto)
+Author URI: http://www.rains.jp/
+Version: 1.0.0
+License: GPLv2
+Text Domain: rains-mmsn
+Domain Path: /languages/
+*/
+/*
+  Copyright 2013 Rains
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+$ra_mmsn_lang = dirname(plugin_basename(__FILE__)) . "/languages/";
+load_plugin_textdomain("rains-mmsn", false, $ra_mmsn_lang);
+
+function ra_modify_multibyte_slug_newpost($slug, $post_ID, $post_status, $post_type) {
+
+  $target_status = array('draft', 'pending', 'auto-draft');
+  $post_raw_status = get_post_status($post_ID);
+
+  if(($post_status != '' && $post_raw_status == 'draft') || in_array($post_raw_status, $target_status)) {
+      if(preg_match('/(%[0-9a-f]{2})+/', $slug)) {
+          $slug = utf8_uri_encode($post_type) . '-' . $post_ID;
+      }
+  }
+
+  return $slug;
+  
+}
+add_filter('wp_unique_post_slug', 'ra_modify_multibyte_slug_newpost', 10, 4 );
---
+++ readme.txt
@@ -0,0 +1,21 @@
+=== RA - Mod Multibyt Slug ===
+Contributors: Rasin (skuraomoto)
+Tags: rains, slug, multibyte, Japanese
+Requires at least: 4.0
+Tested up to: 4.0
+Stable tag: 1.0.0
+
+When the multi-byte character is used in slug of new creation post, it changes so that post_type and post_ID may be used.
+
+== Description ==
+When the multi-byte character is used in slug of new creation post, it changes so that post_type and post_ID may be used.
+
+== Installation ==
+1. Upload the "ra-modify-multibyte-slug-newpost" folder to the "/wp-content/plugins/" directory.
+2. Activate the plugin through the Plugins menu in WordPress.
+
+== Changelog ==
+
+= 1.0.0 =
+* Registration to the plug-in directory.
+* First release.
